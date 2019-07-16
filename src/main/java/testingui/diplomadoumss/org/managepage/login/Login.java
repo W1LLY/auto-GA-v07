6bd1d9d80a3dcd96b988773046be0d550e893d1f
juan-m-m <juan-es20@hotmail.com>
2019-07-16 18:59:27 -0400

    task

--- src/main/java/testingui/diplomadoumss/org/managepage/login/Login.java
+++ src/main/java/testingui/diplomadoumss/org/managepage/login/Login.java
@@ -1,9 +1,10 @@
 package testingui.diplomadoumss.org.managepage.login;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import testingui.diplomadoumss.org.managepage.BasePage;
+import testingui.diplomadoumss.org.managepage.dashboard.Dashboard;
+import testingui.diplomadoumss.org.utilsfiles.PropertyAccesor;
 
 /**
  * @author Marcelo Garay
@@ -11,10 +12,41 @@ import testingui.diplomadoumss.org.managepage.BasePage;
  */
 public class Login extends BasePage {
 
-    @FindBy(xpath = "//input[@name='email' and @type='email']")
+    @FindBy(xpath = "//input[@name='email' and @type='text']")
     private WebElement emailTextField;
+
+    @FindBy(xpath = "//input[@name='password' and @type='password']")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement buttonField;
+
+    public Dashboard setCredentials() throws InterruptedException {
+        setEmail(PropertyAccesor.getInstance().getEmail());
+        setPassword(PropertyAccesor.getInstance().getPassword());
+        pressLogin();
+        Thread.sleep(20000);
+        return new Dashboard();
+    }
+
+    public void initBrowser() {
+        webDriver.get(PropertyAccesor.getInstance().getURL());
+    }
 
     public void setEmail(String email){
         emailTextField.sendKeys(email);
     }
+
+    public void setPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void pressLogin() {
+        buttonField.click();
+    }
+
+    public void finish() {
+        webDriver.quit();
+    }
+
 }
