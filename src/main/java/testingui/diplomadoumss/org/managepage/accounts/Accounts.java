6f4004bc344ae1daa785691583116a35fa9d36b6
Yerko Manzel Peralta <yerko.mp22@gmail.com>
2019-07-19 17:57:21 -0400

    ¨accounts

---
+++ src/main/java/testingui/diplomadoumss/org/managepage/accounts/Accounts.java
@@ -0,0 +1,11 @@
+package testingui.diplomadoumss.org.managepage.accounts;
+
+import testingui.diplomadoumss.org.managepage.BasePage;
+
+/**
+ * @author Yerko Manzel
+ */
+public class Accounts extends BasePage {
+
+
+}
--- src/main/java/testingui/diplomadoumss/org/managepage/dashboard/Dashboard.java
+++ src/main/java/testingui/diplomadoumss/org/managepage/dashboard/Dashboard.java
@@ -5,6 +5,7 @@ import static testingui.diplomadoumss.org.manageevents.Event.*;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import testingui.diplomadoumss.org.managepage.BasePage;
+import testingui.diplomadoumss.org.managepage.accounts.Accounts;
 
 /**
  * @author Marcelo Garay
@@ -18,6 +19,15 @@ public class Dashboard extends BasePage {
     @FindBy(xpath = "//ul[@id='social-sidebar-menu']//a[contains(., 'Cars') and @aria-expanded='false']")
     private WebElement carsExpand;
 
+    @FindBy(xpath = "//ul[@id='social-sidebar-menu']//a[contains(., 'Accounts') and @aria-expanded='false']")
+    private WebElement accountsExpand;
+
+    @FindBy(xpath = "//a[@href='#ACCOUNTS']")
+    private WebElement accounts;
+
+    @FindBy(xpath = "//a[text()='Customers']")
+    private WebElement customersLink;
+
     public Dashboard() {
 //        isWebElementVisible(bookingLink);
         avoidToUse(3);
@@ -25,6 +35,24 @@ public class Dashboard extends BasePage {
 
     public Dashboard clickCarsExpand() {
         clickWebElement(carsExpand);
+        return this;
+    }
+
+    public Dashboard clickAccounts() {
+        clickWebElement(accounts);
+        return this;
+    }
+
+
+    public Accounts clickCustomerLink() {
+        avoidToUse(1);
+        isWebElementVisible(customersLink);
+        clickWebElement(customersLink);
+        return new Accounts();
+    }
+
+    public Dashboard clickAccountsExpand() {
+        clickWebElement(accountsExpand);
         return this;
     }
 }
