d30d5c670039571a5d4206d6a4bdc3e151e70233
Willy Sanchez Rojas <willy.sanchez@dharbor.com>
2019-08-04 21:44:19 -0400

    resolve conflicts

--- src/test/java/testingui/diplomadoumss/org/stepdefinitions/generalStepdefs.java
+++ src/test/java/testingui/diplomadoumss/org/stepdefinitions/generalStepdefs.java
@@ -2,23 +2,78 @@ package testingui.diplomadoumss.org.stepdefinitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import testingui.diplomadoumss.org.managepage.accounts.AccountsCustomers;
 import testingui.diplomadoumss.org.managepage.accounts.GuestCustomers;
 import testingui.diplomadoumss.org.managepage.cars.CarsCars;
 import testingui.diplomadoumss.org.managepage.cars.CarsExtras;
 import testingui.diplomadoumss.org.managepage.cars.CarsSettings;
 import testingui.diplomadoumss.org.managepage.dashboard.Dashboard;
-import testingui.diplomadoumss.org.managepage.login.Login;
 
 /**
- * @author Juan Montaño
+ * @author Willy Sanchez
  */
 public class generalStepdefs {
-    private Login login;
     private Dashboard dashboard = new Dashboard();
     private CarsExtras carsExtras;
     private CarsCars carsCars;
     private CarsSettings settings;
     private GuestCustomers guestCustomers;
+    private AccountsCustomers accountsCustomers;
+
+    @And("^click on option 'Customers'$")
+    public void clickOnOptionCustomers() throws Throwable {
+        accountsCustomers = dashboard.clickOnCustomers();
+    }
+
+    @And("^click on option twenty five in list$")
+    public void clickOnOptionTwentyFiveInList() throws Throwable {
+        accountsCustomers.clickOn25Option();
+    }
+
+    @And("^click on option delete in any customer of list$")
+    public void clickOnOptionDeleteInAnyCustomerOfList() throws Throwable {
+        accountsCustomers.clickOnDeleteOption();
+    }
+
+    @And("^click on Aceptar in confirmation panel$")
+    public void clickOnAceptarInConfirmationPanel() throws Throwable {
+        accountsCustomers.clickOnAceptarOption();
+    }
+
+    @And("^click on Cancelar in confirmation panel$")
+    public void clickOnCancelarInConfirmationPanel() throws Throwable {
+        accountsCustomers.clickOnCancelarOption();
+    }
+
+    @And("^click on option edit in any customer of list$")
+    public void clickOnOptionEditInAnyCustomerOfList() throws Throwable {
+        accountsCustomers.clickOnEditCustomer();
+    }
+
+    @And("^click on option 'Add'$")
+    public void clickOnOptionAdd() throws Throwable {
+        accountsCustomers.clickAddCustomer();
+    }
+
+    @And("^click on button Add in form to create customer$")
+    public void clickOnButtonAddInFormToCreateCustomer() throws Throwable {
+        accountsCustomers.clickOnAddButtonToCreateCustomer();
+    }
+
+    @And("^click on option one hundred in list$")
+    public void clickOnOptionOneHundredInList() throws Throwable {
+        accountsCustomers.clickOn100Option();
+    }
+
+    @And("^click on option 'All' in list$")
+    public void clickOnOptionAllInList() throws Throwable {
+        accountsCustomers.clickOnAllOption();
+    }
+
+    @And("^select any country on form edit customer$")
+    public void selectAnyCountryOnFormEditCustomer() throws Throwable {
+        accountsCustomers.clickOnAnyCountry();
+    }
 
     @And("^click 'CARS' link on 'Left Panel'$")
     public void clickCARSLinkOnLeftPanel() throws Throwable {
